b5c56c140c7d77b2934cf958a3cdd8d72aa0576f
Muskan2314 <161997385+Muskan2314@users.noreply.github.com>
2024-04-11 09:07:46 +0000

    datatype of summary

---
+++ 01-basic/comparison.js
@@ -0,0 +1,20 @@
+//console.log(2 > 1);
+//console.log(2 >= 1);
+//console.log(2 < 1);
+//console.log(2 <= 1);
+//console.log(2 == 1);
+//console.log(2 =! 1);
+
+
+//console.log("2" > 1);
+//console.log("02" > 1);
+
+//console.log(null > 0);
+//console.log(null == 0);
+//console.log(null >= 0);
+//console.log(null != 0);
+
+
+//null kbhi NAN me convert hota hai or kabhi 0 me
+
+
