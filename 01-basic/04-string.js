190230b457a7c916d3efc0d114b7bcbffb6a991b
Muskan2314 <161997385+Muskan2314@users.noreply.github.com>
2024-04-12 10:22:33 +0000

    stringconcept of js

---
+++ 01-basic/04-string.js
@@ -0,0 +1,7 @@
+// STRING CONCINATE
+
+const name = "muskan"
+const repoCount = 50
+//console.log(name + repCount + "Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
